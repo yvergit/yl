b8f88f086ad6e3ae7d182de76ebb81994e502250
yvergit <yvercloud@icloud.com>
2025-04-10 03:33:02 +0200

    sidebar

--- src/store.js
+++ src/store.js
@@ -1,19 +1,20 @@
-import { proxy } from 'valtio'
+import { proxy } from "valtio";
 
 const state = proxy({
   intro: true,
   colors: [
-    '#ccc',
-    '#EFBD4E',
-    '#80C670',
-    '#726DE8',
-    '#EF674E',
-    '#353934',
-    'cyan'
+    "#ccc",
+    "#EFBD4E",
+    "#80C670",
+    "#726DE8",
+    "#EF674E",
+    "#353934",
+    "cyan",
   ],
-  decals: ['react', 'three2', 'pmndrs'],
-  selectedColor: '#EFBD4E',
-  selectedDecal: 'three2'
-})
+  decals: ["react", "three2", "pmndrs"],
+  selectedColor: "#EFBD4E",
+  selectedDecal: "three2",
+  selectedModel: "shirt",
+});
 
-export { state }
+export { state };
