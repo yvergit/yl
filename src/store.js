2cab03cbda5f57060e2d966055272bc5fe5d05a0
ThijnBr <62210824+ThijnBr@users.noreply.github.com>
2025-04-18 16:25:31 +0200

    kleur changes

--- src/store.js
+++ src/store.js
@@ -2,11 +2,25 @@ import { proxy } from "valtio";
 
 const state = proxy({
   intro: true,
-  colors: ["black", "white", "yellow", "lightgreen", "purple", "red", "silver", "cyan", "blue", "violet", "forestgreen", "pink", "#FF7F00"],
+  colors: [
+    { code: "black", name: "Black" },
+    { code: "white", name: "White" },
+    { code: "yellow", name: "Yellow" },
+    { code: "lightgreen", name: "Light Green" },
+    { code: "purple", name: "Purple" },
+    { code: "red", name: "Red" },
+    { code: "silver", name: "Silver" },
+    { code: "cyan", name: "Cyan" },
+    { code: "blue", name: "Blue" },
+    { code: "violet", name: "Violet" },
+    { code: "forestgreen", name: "Forest Green" },
+    { code: "pink", name: "Pink" },
+    { code: "#FF7F00", name: "Holland" }
+  ],
   decals: ["react", "three2", "pmndrs"],
   selectedColor: "white",
-  selectedDecal: "pmndrs",
   selectedModel: "yoga_pants",
+  selectedDecal: "pmndrs",
   decalPosition: [0, 0.04, 0.15],
   decalRotation: [0, 0, 0],
   decalScale: 0.15,
